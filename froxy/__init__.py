dd01a473f7b477a516a816f2804a22e004bc0397
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-09-06 20:40:32 -0300

    Update docstring of froxy module

--- froxy/__init__.py
+++ froxy/__init__.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
-"""Froxy is a module for getting and filtering proxies."""
+"""
+Module for getting and filtering proxies.
+
+This module consumes the API located at: https://github.com/clarketm/proxy-list and
+filters data and provides an interface for using proxies.
+
+Example:
+```
+>>> from froxy import Froxy
+>>> froxy = Froxy()
+>>> froxy.get()
+# Output
+[['255.255.255.255', '3000', ['US', 'N', 'S!', '+'], ...]
+```
+"""
 
 __all__ = ['Froxy']
 
